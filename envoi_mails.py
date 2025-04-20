bad4c6a8cfb3d767c89d108450b4e6d8bbc29988
owenvary <owen.vary@isen-ouest.yncrea.fr>
2025-04-20 14:14:54 +0200

    Met à jour .gitignore et supprime les fichiers d'environnement virtuel de Git

---
+++ envoi_mails.py
@@ -0,0 +1,145 @@
+import smtplib
+import json
+import re
+
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+import os
+
+class EnvoiPlanning:
+    def __init__(self, window):
+        self.window = window
+        self.BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.PLANNINGS_PDF_DIR = os.path.join(self.BASE_DIR, "Data", "Plannings_pdf")
+        self.EMPLOYEES_FILE = os.path.join(self.BASE_DIR, "Data", "Employes_json", "employees.json") # Chemin complet du fichier JSON
+        self.MAILS_FILE = os.path.join(self.BASE_DIR, "mails.json")  # Chemin du fichier mails.json
+
+        self.semaine_selected = self.get_semaine()
+
+        self.dico_date_planning = {
+            "semaine": "",
+            "jours": "",
+            "annee": "",
+        }
+
+    def get_semaine(self):
+        return self.window.semaine_selected
+
+    def get_donnees_employes(self):
+        if os.path.exists(self.EMPLOYEES_FILE):
+            with open(self.EMPLOYEES_FILE, "r", encoding="utf-8") as f:
+                return json.load(f)
+        return []
+
+    def extraire_mails(self):
+        return [
+            {"email": employe.get("email", "")}
+            for employe in self.get_donnees_employes()
+            if employe.get("email")
+        ]
+
+    def get_dates_mails(self, semaine_selected):
+
+        if semaine_selected and isinstance(semaine_selected, str): #semaine selected : "semaine XX - du DD/MM au DD/MM - YYYY"
+            parties = re.split(" - ", semaine_selected)
+            self.dico_date_planning["semaine"] = parties[0]
+            print(self.dico_date_planning["semaine"])
+            self.dico_date_planning["jours"] = parties[1]
+            print(self.dico_date_planning["jours"])
+            self.dico_date_planning["annee"] = parties[2]
+            print(self.dico_date_planning["annee"])
+            return
+        else:
+            return "Erreur lors de la lecture de la semaine choisie"
+
+
+    def get_email_config(self):
+        # Charger la configuration du fichier mails.json
+        if os.path.exists(self.MAILS_FILE):
+            with open(self.MAILS_FILE, "r", encoding="utf-8") as f:
+                config = json.load(f)
+                return config
+        else:
+            print("Le fichier mails.json est manquant ou invalide.")
+            return None
+
+    def send_email_with_pdf(self):
+        # Obtenez les informations de configuration depuis mails.json
+        self.get_dates_mails(self.semaine_selected)
+        email_config = self.get_email_config()
+        if email_config is None:
+            return
+
+        email_envoi = email_config.get("email", "")
+        mdp = email_config.get("pwd", "")
+        serveur = email_config.get("serveur", "")
+
+        try:
+            port = int(email_config.get("port", 465))
+        except (ValueError, TypeError):
+            print("Le port SMTP doit être un entier.")
+            return
+        #port = int(email_config.get("port", 465))
+
+        if not email_envoi or not mdp or not serveur or not port:
+            print("Les informations de configuration du serveur SMTP sont incomplètes.")
+            return 0
+
+        # Générer le nom du fichier PDF avec la semaine et l'année
+        pdf_file_name = f"planning_{self.dico_date_planning['semaine'].replace(' ', '')}_{self.dico_date_planning['annee']}.pdf"  # Exemple: planning_semaine16_2025
+        pdf_file_path = os.path.join(self.PLANNINGS_PDF_DIR, pdf_file_name)
+
+        # Créer un message MIME multipart
+        message = MIMEMultipart()
+        message["From"] = email_envoi
+        # Récupérer tous les e-mails des employés
+        emails_destinataires = [employe["email"] for employe in self.extraire_mails()]
+        email_vice_dir = "fleck.sophie@gmail.com"
+        emails_destinataires.append(email_vice_dir)
+        message["To"] = ", ".join(emails_destinataires)  # Placer tous les e-mails dans le champ "To"
+        message["Subject"] = f"Planning de la {self.dico_date_planning['semaine']}"
+
+        # Texte du message avec les informations de la semaine
+        mail_pro = "patrice_vary@franchise.carrefour.com"
+        tel_pro = "0625538184"
+        message_html = f"""
+        <html>
+          <body>
+            <p>Bonjour,</p>
+            <p>
+              Veuillez trouver ci-joint le planning de la <strong>{self.dico_date_planning['semaine']}</strong>
+              pour la période <strong>{self.dico_date_planning['jours']}</strong> de l'année
+              <strong>{self.dico_date_planning['annee']}</strong>.
+            </p>
+            <p>Cordialement,<br>
+            Patrice VARY</p>
+            <hr>
+            <p><em>Merci de ne pas répondre à ce mail. Pour toute question, veuillez envoyer un mail à l'adresse suivante :</em>
+            <strong> {mail_pro}</strong>,<br>
+            <em>ou contacter le</em> <strong>{tel_pro}</strong>.</p>
+          </body>
+        </html>
+        """
+
+        message.attach(MIMEText(message_html, "html"))
+
+
+        # Vérifier si le fichier PDF existe avant de l'envoyer
+        if os.path.exists(pdf_file_path):
+            try:
+                # Joindre le fichier PDF à l'email
+                with open(pdf_file_path, "rb") as pdf_file:
+                    pdf_part = MIMEApplication(pdf_file.read(), _subtype="pdf")
+                    pdf_part.add_header('Content-Disposition', 'attachment', filename=pdf_file_name)
+                    message.attach(pdf_part)
+
+                # Connexion au serveur SMTP de Gmail et envoi de l'e-mail
+                with smtplib.SMTP_SSL(serveur, port) as smtp_server:
+                    smtp_server.login(email_envoi, mdp)
+                    smtp_server.sendmail(email_envoi, emails_destinataires, message.as_string())
+                    print("E-mail envoyé avec succès avec la pièce jointe !")
+            except Exception as e:
+                print(f"Une erreur est survenue lors de l'envoi du mail : {e}")
+        else:
+            print(f"Le fichier PDF {pdf_file_name} n'existe pas à l'emplacement spécifié.")
---
+++ gestion_employes.py
@@ -0,0 +1,167 @@
+
+import json
+import os
+
+
+from PySide6.QtGui import QFont
+from PySide6.QtWidgets import ( QTableWidget, QPushButton,
+    QTableWidgetItem, QHeaderView, QVBoxLayout, QMessageBox, QDialog
+)
+
+
+
+class GestionEmployes(QDialog):
+
+
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # Header + dimension fenêtre
+        self.setWindowTitle("Liste des employés")
+        self.setMinimumSize(800, 600)
+
+        self.BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Répertoire de base
+        self.DATA_DIR = os.path.join(self.BASE_DIR, "Data", "Employes_json")  # Dossier des employés
+        self.EMPLOYEES_FILE = os.path.join(self.DATA_DIR, "employees.json")  # Chemin complet du fichier JSON
+
+        # Police utilisée
+        self.setFont(QFont("Segoe UI", 12))
+
+        # Instance interface
+        self.layout = QVBoxLayout(self)
+
+        # Création du tableau nx2
+        self.table = QTableWidget()
+        self.table.setColumnCount(2)
+
+        # Headers tableau
+        self.table.setHorizontalHeaderLabels(["Nom", "Email"])
+        self.table.horizontalHeader().setStretchLastSection(True)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        # Intégration du tableau
+        self.layout.addWidget(self.table)
+
+        # Création des boutons
+        self.btn_ajouter = QPushButton("Ajouter un employé")
+        self.btn_supprimer = QPushButton("Supprimer l'employé sélectionné")
+        self.btn_sauvegarder = QPushButton("Sauvegarder les modifications")
+
+        # Intégration des boutons
+        for btn in (self.btn_ajouter, self.btn_sauvegarder, self.btn_supprimer):
+            self.layout.addWidget(btn)
+
+        # EventListeners : btn -> fonctions
+        self.btn_ajouter.clicked.connect(self.ajouter_employe)
+        self.btn_supprimer.clicked.connect(self.supprimer_employe)
+        self.table.itemChanged.connect(self.mettre_a_jour_donnees)
+        self.btn_sauvegarder.clicked.connect(self.sauvegarder_et_fermer)
+
+        # Charger les données depuis le JSON
+        self.employes = []
+        self.load_employes()
+
+    def sauvegarder_et_fermer(self):
+        """ Assure la fermeture et déclenche la sauvegarde des données employés après que btn_sauvegatder ait été cliqué."""
+        self.save_employes()
+        QMessageBox.information(self, "Succès", "Employés enregistrés avec succès.")
+        self.accept()
+
+    def load_employes(self):
+        """Charge les employés depuis EMPLOYEES_FILE, initialise une liste vide si le fichier est inexistant."""
+        # Vérifier si le dossier existe, sinon le créer
+        directory = os.path.dirname(self.EMPLOYEES_FILE)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        # Vérifier si le fichier existe puis l'ouvrir en mode lecture
+        if os.path.exists(self.EMPLOYEES_FILE):
+            try:
+                with open(self.EMPLOYEES_FILE, "r", encoding="utf-8") as f:
+                    self.employes = json.load(f)
+            # Génère un pop-up si le chargement à échouer
+            except Exception as e:
+                QMessageBox.warning(self, "Erreur", f"Impossible de charger les employés : {e}")
+                self.employes = []
+        else:
+            # Si le fichier n'existe pas, on l'initialise avec une liste vide
+            self.employes = []
+
+        # Mise à jour du tableau
+        self.update_table()
+
+    def update_table(self):
+        """Remplit le tableau avec la liste actuelle d'employés."""
+        self.table.blockSignals(True)  # Éviter qu'une fonction se déclenche due à un changement dans une cellule
+        self.table.setRowCount(len(self.employes))
+        for row, employe in enumerate(self.employes):
+            self.table.setItem(row, 0, QTableWidgetItem(employe.get("nom", ""))) # Chaîne vide par défault
+            self.table.setItem(row, 1, QTableWidgetItem(employe.get("email", "")))
+        self.table.blockSignals(False) # Réactiver les signaux
+
+    def save_employes(self):
+        """Sauvegarde les employés dans le fichier JSON."""
+        try:
+            with open(self.EMPLOYEES_FILE, "w", encoding="utf-8") as f:
+                # ensure_ascii = False afin d'éviter les problèmes avec les accents
+                json.dump(self.employes, f, ensure_ascii=False, indent=4)
+        except Exception as e:
+            QMessageBox.warning(self, "Erreur", f"Erreur lors de la sauvegarde : {e}")
+
+    def get_noms_employes(self):
+        return [employe["nom"] for employe in self.employes]
+
+    def ajouter_employe(self):
+        """Ajoute un nouvel employé par défaut au tableau. Création, mise à jour puis sauvegarde"""
+        self.employes.append({"nom": "Nouvel Employé", "email": "nouveau@mail.com"})
+        self.update_table()
+        self.save_employes()
+
+    def supprimer_employe(self):
+        """Supprime l'employé sélectionné après confirmation du pop-up"""
+        row = self.table.currentRow()
+        if row >= 0:
+            confirm = QMessageBox.question(
+                self, "Confirmation", "Supprimer cet employé ?",
+                QMessageBox.Yes | QMessageBox.No
+            )
+            if confirm == QMessageBox.Yes:
+                self.employes.pop(row)
+                self.update_table()
+                self.save_employes()
+        else:
+            QMessageBox.warning(self, "Alerte", "Aucun employé sélectionné.")
+
+    def mettre_a_jour_donnees(self, item):
+        """Met à jour les données internes quand une cellule du tableau est modifiée."""
+        row = item.row()
+        col = item.column()
+
+        if row < len(self.employes):
+            if col == 0:
+                self.employes[row]["nom"] = item.text()
+            elif col == 1:
+                self.employes[row]["email"] = item.text()
+
+            # Enregistre les modifications dans le fichier JSON
+            with open(self.EMPLOYEES_FILE, 'w', encoding='utf-8') as f:
+                json.dump(self.employes, f, indent=4, ensure_ascii=False)
+
+            #QMessageBox.information(self, "Succès", "Employés enregistrés avec succès.")
+
+    def charger_employes(self):
+        """
+        Met à jour le tableau noms - mails des employés après avoir chargé els données depuis le JSON
+        """
+        path = os.path.join(self.BASE_DIR, "Data", "employes.json")
+        if os.path.exists(path):
+            with open(path, "r", encoding="utf-8") as f:
+                employes = json.load(f)
+
+            self.table.setRowCount(0)
+            for nom in employes:
+                row_position = self.table.rowCount()
+                self.table.insertRow(row_position)
+                self.table.setItem(row_position, 0, QTableWidgetItem(nom))
+            self.update_table()
